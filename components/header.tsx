6c11d807cac20d9ec60131466b9b3c2413280ae0
gustavo <gustavo@caffelli.com>
2021-12-10 01:29:47 -0500

    [builder-header] Added dark mode feature

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,20 @@
 import cn from "classnames"
 import NavLink from "./nav-link"
+import ToggleThemeButton from './toggle-theme-button'
 
 const Header = () => {
   return (
     <header className={cn("absolute inset-x-0 bottom-0 sm:inset-x-0 sm:top-0")}>
       <div
         className={cn(
-          "w-full h-0.5 sm:h-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+          "w-full h-0.5 sm:h-1 bg-gradient-to-r from-yellow-500 via-lime-500 to-emerald-500"
         )}
       ></div>
-      <div className={cn("py-4 sm:py-8 flex justify-end mx-2 sm:mx-16")}>
-        <nav className={cn("flex space-x-4 sm:space-x-8")}>
+      <div className={cn("py-4 sm:py-8 flex justify-evenly sm:justify-end mx-2 sm:mx-16")}>
+        <nav className={cn("flex items-center space-x-4 sm:space-x-8")}>
           <NavLink to="/" name="home" />
           <NavLink to="/projects" name="projects" />
-          <div className="text-white">Change</div>
+          <ToggleThemeButton />
         </nav>
       </div>
     </header>
