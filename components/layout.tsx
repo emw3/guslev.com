1496e0e08d65e4a1e54285ddef13defb5d957d34
emw3 <emw3wme@gmail.com>
2021-12-11 01:05:20 -0500

    [build-bio] Added temp bio

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,23 @@
-import cn from 'classnames'
-import Meta from './meta'
-import Header from './header'
+import cn from "classnames";
+import Meta from "./meta";
+import Header from "./header";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export default function Layout({ children }: Props) {
   return (
     <>
       <Meta />
-      <div className={cn('min-h-screen bg-white dark:bg-zinc-900')}>
+      <div
+        className={cn(
+          "min-h-screen bg-slate-50 dark:bg-zinc-900 relative sm:pt-32 text-zinc-900 dark:text-slate-50"
+        )}
+      >
         <Header />
-        <main>{children}</main>
+        <main className={cn("z-10")}>{children}</main>
       </div>
     </>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,50 @@
-import type { NextPage } from 'next'
-import cn from 'classnames'
-import Head from 'next/head'
-import Container from '../components/container'
-import Layout from '../components/layout'
-import { WEBSITE_TITLE } from '../lib/constants'
-
+import type { NextPage } from "next";
+import cn from "classnames";
+import Head from "next/head";
+import Container from "../components/container";
+import Layout from "../components/layout";
+import { WEBSITE_TITLE } from "../lib/constants";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout>
         <Head>
-          <title>{WEBSITE_TITLE} - </title>
+          <title>{WEBSITE_TITLE}</title>
         </Head>
         <Container>
-          <h1>Home</h1>
+          <div className="">
+            <div className="py-16">
+              <p className={cn("font-mono text-xs sm:text-base mb-1 sm:mb-2")}>
+                Hello! Im
+              </p>
+              <h1
+                className={cn(
+                  "text-3xl sm:text-5xl font-mono font-extrabold mb-4 sm:mb-6"
+                )}
+              >
+                Gustavo Levano.
+              </h1>
+              <div
+                className={cn(
+                  "inline-flex transition ease-in-out delay-150 skew-y-3 hover:skew-y-0 p-1 sm:p-2 bg-zinc-900 text-slate-50 dark:bg-slate-50 dark:text-zinc-900"
+                )}
+              >
+                <h2 className={cn("text-xl sm:text-3xl font-mono font-bold")}>
+                  Full-Stack Developer
+                </h2>
+              </div>
+            </div>
+            <div className={cn('mt-8')}>
+              <p className={cn('font-sans antialiased text-base sm:text-lg tracking-normal')}>
+                Im a full-stack developer with a passion for building web and mobile applications. Ive built a number of apps for clients and have worked on a number of projects.
+              </p>
+            </div>
+          </div>
         </Container>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
