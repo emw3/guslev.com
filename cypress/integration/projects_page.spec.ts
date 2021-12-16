753e2d752fe18717eaad48efbde7f93c515c16ed
emw3 <emw3wme@gmail.com>
2021-12-16 00:20:51 -0500

    [testing] Updated integration test for Projects Page

--- cypress/integration/projects_page.spec.ts
+++ cypress/integration/projects_page.spec.ts
@@ -5,6 +5,6 @@ describe('Projects Page', () => {
     cy.visit('/')
     cy.get('a[href*="projects"]').click()
     cy.url().should('include', '/projects')
-    cy.get('h1').contains('under construction')
+    cy.get('h2').contains('Personal Website')
   })
 })
